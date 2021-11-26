c9777f1a344c201ad8aa1f97e0286a818ddf6077
Kaan Yıldırım <kaan.yildirim@turkcell.com.tr>
2021-11-26 05:18:17 +0300

    Unsubscribe removed. Not needed

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -29,7 +29,6 @@ final class ViewController: UIViewController,UIImagePickerControllerDelegate, UI
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        unsubscribeToKeyboardNotifications()
     }
 
     private func prepareTextFields() {
@@ -50,11 +49,6 @@ final class ViewController: UIViewController,UIImagePickerControllerDelegate, UI
     private func subscribeToKeyboardNotifications() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-
-    private func unsubscribeToKeyboardNotifications() {
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 
     @objc func keyboardWillShow(_ notification: Notification) {
